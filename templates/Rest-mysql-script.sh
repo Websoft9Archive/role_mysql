d6c3fa4a1cb62f8eba75fc95e885369fbab56e21
Mozlk <xu_junkai@foxmail.com>
2020-02-14 20:39:53 +0800

    123

--- templates/Rest-mysql-script.sh
+++ templates/Rest-mysql-script.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 systemctl stop mysqld
 mysqld --console --skip-grant-tables  --user=mysql &
+sleep 5
 {% if mysql_version <= "5.6" %}
-mysql -e "use mysql;update user set password=password('{{mysql_root_password}}') where user='root';"
-ps aux | grep "mysqld" |grep -v grep| cut -c 9-15 | xargs kill -9
+mysql -e "use mysql;update user set password=password('') where user='root';"
 {% endif %}
-{% if mysql_version == "7.0" %}
-mysql -e "use mysql;update user set authentication_string = password('{{mysql_root_password}}') where user = 'root';"
-ps aux | grep "mysqld" |grep -v grep| cut -c 9-15 | xargs kill -9
+{% if mysql_version == "5.7" %}
+mysql -e "use mysql;update user set authentication_string = password('') where user = 'root';"
 {% endif %}
 {% if mysql_version == "8.0" %}
 mysql -e "use mysql;update user set authentication_string = '' where user = 'root';"
-ps aux | grep "mysqld" |grep -v grep| cut -c 9-15 | xargs kill -9
-systemctl start mysqld
-mysql -uroot --password='' -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '{{mysql_root_password}}'" --connect-expired-password
 {% endif %}
+ps aux | grep "mysqld" |grep -v grep| cut -c 9-15 | xargs kill -9
 systemctl restart mysqld
