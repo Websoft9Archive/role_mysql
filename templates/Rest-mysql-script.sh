28fdb154cb8320bccaa0dcf4ec9ac5582d56b940
Mozlk <xu_junkai@foxmail.com>
2020-02-14 17:04:28 +0800

    123

--- templates/Rest-mysql-script.sh
+++ templates/Rest-mysql-script.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 systemctl stop mysqld
 mysqld --console --skip-grant-tables  --user=mysql &
-
 {% if mysql_version <= "5.6" %}
 mysql -e "use mysql;update user set password=password('{{mysql_root_password}}') where user='root';"
 ps aux | grep "mysqld" |grep -v grep| cut -c 9-15 | xargs kill -9
-
 {% endif %}
 {% if mysql_version == "7.0" %}
 mysql -e "use mysql;update user set authentication_string = password('{{mysql_root_password}}') where user = 'root';"
 ps aux | grep "mysqld" |grep -v grep| cut -c 9-15 | xargs kill -9
 {% endif %}
-
 {% if mysql_version == "8.0" %}
 mysql -e "use mysql;update user set authentication_string = '' where user = 'root';"
 ps aux | grep "mysqld" |grep -v grep| cut -c 9-15 | xargs kill -9
 systemctl start mysqld
 mysql -uroot --password='' -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '{{mysql_root_password}}'" --connect-expired-password
 {% endif %}
-
 systemctl restart mysqld
