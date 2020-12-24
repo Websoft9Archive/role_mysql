6ba16911c229e2f2544786a728d7a168520612b2
qiaofeng1227 <76487013@qq.com>
2020-12-24 10:53:58 +0800

    Update reset_password.sh

--- tools/reset_password.sh
+++ tools/reset_password.sh
@@ -4,7 +4,7 @@ sudo systemctl stop mysql;
 sudo sed -i 's/validate_password/#&/' /etc/my.cnf;
 sudo sed -i '/\[mysqld\]/a skip-grant-tables'   /etc/my.cnf;
 sudo systemctl start mysql
-mysql_root_password=$(pwgen -ncCs 15 1)
+mysql_root_password="$(pwgen -nscCy -r "()[]<>,#{}\`\\'\"|/" 15 1)"
 version=$(mysql --version)
 flag=0;
 if [[  $version =~ "8.0" ]]
@@ -17,30 +17,31 @@ then
 	update user set host='%' where user='root';
 	flush privileges;
 EOF
-	flag=1;
-elif [[ $version =~ "10.4"  ]]
-then
-	/usr/bin/mysql -u root mysql << EOF
-	flush privileges;
-	SET password for 'root'@'%'=password('$mysql_root_password');
-	flush privileges;
-EOF
-	flag=1;
-elif [[ $version =~ "5.5" ]] || [[ $(mysql --version) =~ "5.6" ]] || [[ $(mysql --version) =~ "10.1" ]]
+	if [[ $? = 0 ]]
+	then
+		flag=1;
+	fi
+elif [[ $version =~ "5.5" ]] || [[ $(mysql --version) =~ "5.6" ]]
 then
 	/usr/bin/mysql -u root mysql << EOF
 	update user set password = Password('$mysql_root_password') where User = 'root';
 EOF
-	flag=1;
-elif [[ $version =~ "5.7" ]] || [[ $(mysql --version) =~ "10.2" ]] || [[ $(mysql --version) =~ "10.3" ]]
+	if [[ $? = 0 ]]
+        then
+                flag=1;
+        fi
+elif [[ $version =~ "5.7" ]] 
 then
 	/usr/bin/mysql -u root mysql << EOF
 	update user set authentication_string = Password('$mysql_root_password') where User = 'root';
 EOF
-	flag=1;
+	if [[ $? = 0 ]]
+        then
+                flag=1;
+        fi
 fi
 sudo sed -i 's/skip-grant-tables/#&/' /etc/my.cnf
-sudo sed -i 's/#validate_password/validate_password/' /etc/my.cnf
+sudo sed -i 's/#*validate_password/validate_password/' /etc/my.cnf
 sudo systemctl restart mysql
 if [[ $flag == 1 ]]
 then
@@ -48,10 +49,11 @@ then
 	
 	if [[ -s "/credentials/password.txt" ]]
 	then
-		sudo sed -i 's@mysql administrator password:.*@mysql administrator password: '$mysql_root_password'@g' /credentials/password.txt
+		sudo sed -i 's/mysql administrator password:.*/mysql administrator password: '$mysql_root_password'/g' /credentials/password.txt
 	else
-		echo -e "mysql administrator username:root
-mysql administrator password:$mysql_root_password\n\n\n\n" >  /credentials/password.txt
+		sudo mkdir -p /credentials/
+		sudo echo -e "mysql administrator username:root
+mysql administrator password:$mysql_root_password\n\n\n\n" | sudo tee /credentials/password.txt
 	fi
 	
 	echo -e "The password of the newly set mysql root account is saved in the /credentials/password.txt directory, and the new password can be viewed through \`cat /credentials/password.txt\`\n"
