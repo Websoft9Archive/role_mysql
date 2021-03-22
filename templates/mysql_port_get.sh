3e66584f3f51cfd4a75b7af6724354ab5290ac7f
zengxc <zxc916684@163.com>
2021-03-22 14:39:06 +0800

    test

--- templates/mysql_port_get.sh
+++ templates/mysql_port_get.sh
@@ -1,24 +1,21 @@
-port={{mysql_port}}
-container_name=(`docker ps | awk '{print $NF}' | sed 1d | sort`)
+mysql_port=3000
+container_name=($(docker ps | awk '{print $NF}' | sed 1d |sort))
 
-check_mysql_port() {
-   netstat -tlpn | grep "\b$port\b" &>/dev/null
-}
-
-if [ $container_name == "mysql{{mysql_version}}" ];then
-   echo $port
-   exit 0
-fi
-
-if check_mysql_port ! $port;then
-   echo $port
-   exit 0
-fi
-
-while [[ -n $new_port ]]  
+until [ -n "$mysql_new_port" ] 
 do
-   new_port=`ss -ntlp |grep $port |awk '{print $4}' |awk '{print substr($0,length($0)-3)}'`
-   port=`expr $port + 1`
-done
-   echo $new_port
+ if [[ "${container_name[@]}" =~ "mysql{{mysql_port}}" ]]; then
+   mysql_port=$(docker inspect mysql{{mysql_port}} |jq .[].NetworkSettings.Ports |grep HostPort |cut -d'"' -f4)
+   echo $mysql_port
    exit 0
+ else
+   i=1
+   mysql_new_port=`ss -ntlp |grep $mysql_port |awk '{print $4}' |awk '{print substr($0,length($0)-3)}'`
+   mysql_port=`expr $mysql_port + 1`
+ fi
+   let i++
+done
+
+mysql_new_port=`expr $mysql_new_port + $i`
+echo $mysql_new_port
+
+
